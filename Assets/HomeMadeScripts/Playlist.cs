c86d6f431d58c205edb0548fa98e48089a0351ed
Haled <hadrien.navarro@epita.fr>
2017-04-21 20:58:18 +0200

    music finally

---
+++ Assets/HomeMadeScripts/Playlist.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playlist : MonoBehaviour {
+
+    Object[] MyMusic;
+
+    void Awake()
+    {
+        MyMusic = Resources.LoadAll("Music", typeof(AudioClip));
+        GetComponent<AudioSource>().clip = MyMusic[0] as AudioClip;
+    }
+    // Use this for initialization
+    void Start () {
+        GetComponent<AudioSource>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!GetComponent<AudioSource>().isPlaying)
+        {
+            PlayRandomMusic();
+        }
+	}
+
+    void PlayRandomMusic()
+    {
+        GetComponent<AudioSource>().clip = MyMusic[Random.Range(0,MyMusic.Length)] as AudioClip;
+        GetComponent<AudioSource>().Play();
+    }
+}
