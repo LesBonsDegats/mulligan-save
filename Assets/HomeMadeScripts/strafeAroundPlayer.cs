2cf566a1ab79549efc0d7a3aea66084f514de6ac
project-epita <bourdet.jeremie@gmail.com>
2017-04-22 20:31:20 +0200

    nada

---
+++ Assets/HomeMadeScripts/strafeAroundPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class strafeAroundPlayer : MonoBehaviour {
+    public GameObject player;
+    public int distanceMin;
+    public int distanceMax;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
